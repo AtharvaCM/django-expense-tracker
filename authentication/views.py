9b98601346cf6e917a0a87e8d4cfe11afaa39891
AtharvaCM <necrons999@gmail.com>
2021-12-28 09:25:47 +0530

    async username validation

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 
 class RegistrationView(View):
@@ -7,3 +10,19 @@ class RegistrationView(View):
         template_name = 'authentication/register.html'
         context = {}
         return render(request, template_name, context)
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            msg = 'Username should only contain alphanumeric characters!'
+            return JsonResponse({'username_error': msg}, status=400)
+
+        if User.objects.filter(username=username).exists():
+            msg = 'Username already exists, choose another one!'
+            return JsonResponse({'username_error': msg}, status=409)
+
+        return JsonResponse({'username_valid': True})
